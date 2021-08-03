b5cd8ebf2bc6aff36f8cb5a3c489af77cac04632
Marcos Wellerson <m_wellerson@outlook.com>
2021-08-02 22:37:09 -0300

    firt commit

--- source/js/main.js
+++ source/js/main.js
@@ -1,17 +1,49 @@
+import anime from './anime-master/lib/anime.es.js';
+const intro = document.querySelector('#home');
+const enter = document.querySelector('.home-img-arrow');
+const shape = document.querySelector('#Group_7 path');
+shape.style.transformOrigin = '50% 0%';
+
+function animation() {
+	anime({
+		targets: intro,
+		duration: 2100,
+		easing: 'easeInOutSine',
+		translateY: '-200vh'
+	});
+	
+	anime({
+		targets: 'svg path',
+		translateY: [0, 100],
+		delay: function (el, i, l) {
+			return i * 5000
+		},
+		easing: 'easeOutCubic',
+		duration: 4000
+	});
+}
+
+const init = () => {
+	enter.addEventListener('click', animation);
+	enter.addEventListener('touchenter', animation);
+};
+
+init();
+
 const superDiv = document.querySelector('.product-info-super');
 const auto     = document.querySelector('.product-info-auto');
 const product  = document.querySelector('#product');
 
-superDiv.onmouseover = function() {
+superDiv.onmouseover = function () {
 	product.className = 'hovered-green';
 }
-superDiv.onmouseout = function() {
+superDiv.onmouseout = function () {
 	product.className = '';
 }
-auto.onmouseover = function() {
+auto.onmouseover = function () {
 	product.className = 'hovered-auto';
 }
 
-auto.onmouseout = function() {
+auto.onmouseout = function () {
 	product.className = '';
 }
